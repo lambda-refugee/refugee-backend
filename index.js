0aec01be5525820e98dbf529830122af9afe7772
David Situ <davidsitu626@gmail.com>
2019-02-02 12:29:35 -0800

    register endpoint functional, login endpoint pending

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 require('dotenv').config();
 
-const express = require('express');
-// const bcrypt = require('bcryptjs');
-const cors = require('cors');
-
-// const jwt = require('jsonwebtoken');
-
-const server = express();
-
-server.use(express.json());
-server.use(cors());
-
-server.get('/', (req, res) => {
-    res.send('api working')
-})
-
-
+const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
 server.listen(port, function(){
