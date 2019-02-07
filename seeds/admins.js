a8ef8ec62b029455c377f94bc3dec903b334e492
David Situ <davidsitu626@gmail.com>
2019-02-07 09:18:48 -0800

    Attempting to hash seeded password

--- seeds/admins.js
+++ seeds/admins.js
@@ -1,11 +1,12 @@
+const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {username: 'minion', password: '123456'},
+        {username: 'minion', password: bcrypt.hashSync('123456')},
         {username: 'minion2', password:'123456'}
       ]);
     });
